754f59c759b54a76f3b0242483bd51155c4de88f
Manish Sahu <40175566+manishlok@users.noreply.github.com>
2021-06-12 15:59:18 +0545

    aos not working

--- functions.php
+++ functions.php
@@ -141,11 +141,12 @@ add_action( 'widgets_init', 'loknext_widgets_init' );
  */
 function loknext_scripts() {
 	$theme = wp_get_theme();
-	wp_enqueue_style( 'loknext-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_enqueue_style('tailwind-css', tailpress_get_mix_compiled_asset_url('css/app.css'), array(), $theme->get('Version'));
-	wp_enqueue_style('aosv3beta-css', get_template_directory_uri() . '/css/animate.min.css', array(), _S_VERSION, true );
-	wp_enqueue_script( 'loknext-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script('aosv3beta-animation', get_template_directory_uri() . '/js/animate.js', array(), _S_VERSION, true);
+	wp_enqueue_style( 'lokbidhi', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style('app', tailpress_get_mix_compiled_asset_url('css/app.css'), array(), $theme->get('Version'));
+	wp_enqueue_style('animate', get_template_directory_uri() . '/css/animate.min.css', array(), _S_VERSION, true );
+	wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('appjs', get_template_directory_uri() . '/js/app.js', array(), _S_VERSION, true);
+	wp_enqueue_script('animation', get_template_directory_uri() . '/js/animate.js', array(), _S_VERSION, true);
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
