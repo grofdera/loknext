054317b0cefdd4b38fcb960a201d48246686b8d8
Manish Sahu <vegmodelio@gmail.com>
2021-06-16 18:31:23 +0545

    components

---
+++ manish/snipp-front-page.php
@@ -0,0 +1,124 @@
+<div class="items-center sm:flex sm:space-x-8 mt-36">
+    <div data-aos="fade-right" class="relative sm:w-1/2">
+        <div class="absolute z-0 w-12 h-12 bg-yellow-500 rounded-full -left-4 -top-3 animate-pulse"></div>
+        <h1 class="relative z-50 text-2xl font-semibold text-darken lg:pr-10">Everything you can do in a physical classroom, <span class="text-yellow-500">you can do with Skilline</span></h1>
+        <p class="py-5 lg:pr-32">Skilline’s school management software helps traditional and online schools manage scheduling, attendance, payments and virtual classrooms all in one secure cloud-based system.</p>
+        <a href="" class="underline">Learn More</a>
+    </div>
+    <div data-aos="fade-left" class="relative mt-10 sm:w-1/2 sm:mt-0">
+        <div style="background: #23BDEE;" class="absolute z-0 w-24 h-24 rounded-lg floating -top-3 -left-3"></div>
+        <img class="relative z-40 rounded-xl" src="img/lecture.jpg" alt="">
+        <button class="absolute z-50 flex items-center justify-center transition duration-300 ease-in-out transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-14 h-14 top-1/2 left-1/2 focus:outline-none hover:scale-110">
+            <svg class="w-5 h-5 ml-1" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22.5751 12.8097C23.2212 13.1983 23.2212 14.135 22.5751 14.5236L1.51538 27.1891C0.848878 27.5899 5.91205e-07 27.1099 6.25202e-07 26.3321L1.73245e-06 1.00123C1.76645e-06 0.223477 0.848877 -0.256572 1.51538 0.14427L22.5751 12.8097Z" fill="#23BDEE" />
+            </svg>
+        </button>
+        <div class="absolute z-10 w-40 h-40 bg-yellow-500 rounded-lg floating -bottom-3 -right-3"></div>
+    </div>
+</div>
+
+<div class="items-start mt-40 md:flex md:space-x-10">
+    <div data-aos="fade-down" class="relative md:w-7/12">
+        <div style="background: #33EFA0" class="absolute z-0 w-32 h-32 rounded-full left-4 -top-12 animate-pulse"></div>
+        <div style="background: #33D9EF;" class="absolute z-0 w-5 h-5 rounded-full left-36 -top-12 animate-ping"></div>
+        <img class="relative z-50 floating" src="img/vcall.png" alt="">
+        <div style="background: #5B61EB;" class="absolute z-0 rounded-full w-36 h-36 right-16 -bottom-1 animate-pulse"></div>
+        <div style="background: #F56666;" class="absolute z-0 w-5 h-5 rounded-full right-52 bottom-1 animate-ping"></div>
+    </div>
+    <div data-aos="fade-down" class="mt-20 text-gray-500 md:w-5/12 md:mt-0">
+        <h1 class="text-2xl font-semibold text-darken lg:pr-40">A <span class="text-yellow-500">user interface</span> designed for the classroom</h1>
+        <div class="flex items-center my-5 space-x-5">
+            <div class="flex items-center justify-center flex-shrink p-3 bg-white rounded-full shadow-lg">
+                <svg class="w-4 h-4" viewBox="0 0 27 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="11.8182" height="11.8182" rx="2" fill="#2F327D" />
+                    <rect y="14.1816" width="11.8182" height="11.8182" rx="2" fill="#2F327D" />
+                    <rect x="14.7727" width="11.8182" height="11.8182" rx="2" fill="#2F327D" />
+                    <rect x="14.7727" y="14.1816" width="11.8182" height="11.8182" rx="2" fill="#F48C06" />
+                </svg>
+            </div>
+            <p>Teachers don’t get lost in the grid view and have a dedicated Podium space.</p>
+        </div>
+        <div class="flex items-center my-5 space-x-5">
+            <div class="flex items-center justify-center flex-shrink p-3 bg-white rounded-full shadow-lg">
+                <svg class="w-4 h-4" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="8" y="6" width="20" height="20" rx="2" fill="#2F327D" />
+                    <rect width="21.2245" height="21.2245" rx="2" fill="#F48C06" />
+                </svg>
+            </div>
+            <p>TA’s and presenters can be moved to the front of the class.</p>
+        </div>
+        <div class="flex items-center my-5 space-x-5">
+            <div class="flex items-center justify-center flex-shrink p-3 bg-white rounded-full shadow-lg">
+                <svg class="w-4 h-4" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.5 11.375C6.15469 11.375 7.5 9.91758 7.5 8.125C7.5 6.33242 6.15469 4.875 4.5 4.875C2.84531 4.875 1.5 6.33242 1.5 8.125C1.5 9.91758 2.84531 11.375 4.5 11.375ZM25.5 11.375C27.1547 11.375 28.5 9.91758 28.5 8.125C28.5 6.33242 27.1547 4.875 25.5 4.875C23.8453 4.875 22.5 6.33242 22.5 8.125C22.5 9.91758 23.8453 11.375 25.5 11.375ZM27 13H24C23.175 13 22.4297 13.3605 21.8859 13.9445C23.775 15.0668 25.1156 17.093 25.4062 19.5H28.5C29.3297 19.5 30 18.7738 30 17.875V16.25C30 14.4574 28.6547 13 27 13ZM15 13C17.9016 13 20.25 10.4559 20.25 7.3125C20.25 4.16914 17.9016 1.625 15 1.625C12.0984 1.625 9.75 4.16914 9.75 7.3125C9.75 10.4559 12.0984 13 15 13ZM18.6 14.625H18.2109C17.2359 15.1328 16.1531 15.4375 15 15.4375C13.8469 15.4375 12.7688 15.1328 11.7891 14.625H11.4C8.41875 14.625 6 17.2453 6 20.475V21.9375C6 23.2832 7.00781 24.375 8.25 24.375H21.75C22.9922 24.375 24 23.2832 24 21.9375V20.475C24 17.2453 21.5812 14.625 18.6 14.625ZM8.11406 13.9445C7.57031 13.3605 6.825 13 6 13H3C1.34531 13 0 14.4574 0 16.25V17.875C0 18.7738 0.670312 19.5 1.5 19.5H4.58906C4.88438 17.093 6.225 15.0668 8.11406 13.9445Z" fill="#2F327D" />
+                </svg>
+            </div>
+            <p>Teachers can easily see all students and class data at one time.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Tools For Teachers And Learners -->
+<div class="flex flex-col items-center mt-16 md:flex-row md:space-x-10">
+    <div data-aos="fade-right" class="md:w-1/2 lg:pl-14">
+        <h1 class="text-3xl font-semibold text-darken lg:pr-56"><span class="text-yellow-500">Tools</span> For Teachers And Learners</h1>
+        <p class="my-4 text-gray-500 lg:pr-32">Class has a dynamic set of teaching tools built to be deployed and used during class. Teachers can handout assignments in real-time for students to complete and submit.</p>
+    </div>
+    <img data-aos="fade-left" class="md:w-1/2" src="img/girl-with-books.png">
+</div>
+
+<!-- Assessments, Quizzes, Tests -->
+<div class="flex flex-col-reverse items-center mt-20 md:flex-row md:space-x-10">
+    <div data-aos="fade-right" class="md:w-6/12">
+        <img class="md:w-11/12" src="img/true-false.png">
+    </div>
+    <div data-aos="fade-left" class="md:w-6/12 md:transform md:-translate-y-20">
+        <h1 class="text-3xl font-semibold text-darken lg:pr-64">Assessments, <span class="text-yellow-500">Quizzes</span>, Tests</h1>
+        <p class="my-5 text-gray-500 lg:pr-52">Easily launch live assignments, quizzes, and tests. Student results are automatically entered in the online gradebook.</p>
+    </div>
+</div>
+
+<!-- Class Management Tools for Educators -->
+<div class="flex flex-col items-center mt-12 md:flex-row">
+    <div data-aos="fade-right" class="md:w-5/12">
+        <h1 class="text-3xl font-semibold leading-tight text-darken lg:pr-32"><span class="text-yellow-500">Class Management</span> Tools for Educators</h1>
+        <p class="my-5 lg:pr-14">Class provides tools to help run and manage the class such as Class Roster, Attendance, and more. With the Gradebook, teachers can review and grade tests and quizzes in real-time.</p>
+    </div>
+    <img data-aos="fade-left" class="md:w-7/12" src="img/gradebook.png">
+</div>
+
+<!-- One-on-One Discussions -->
+<div class="flex flex-col-reverse items-center mt-24 md:flex-row md:space-x-10">
+    <div data-aos="fade-right" class="md:w-7/12">
+        <img class="md:w-11/12" src="img/discussion.png">
+    </div>
+    <div data-aos="fade-left" class="md:w-5/12 md:transform md:-translate-y-6">
+        <h1 class="text-3xl font-semibold text-darken lg:pr-64">One-on-One <span class="text-yellow-500">Discussions</span></h1>
+        <p class="my-5 text-gray-500 lg:pr-24">Teachers and teacher assistants can talk with students privately without leaving the Zoom environment.</p>
+    </div>
+</div>
+
+<!-- TESTIMONIAL -->
+<div class="flex flex-col-reverse items-start mt-24 md:flex-row md:space-x-10">
+    <div data-aos="zoom-in-right" class="md:w-5/12">
+        <div class="flex items-center mb-5 space-x-20">
+            <span class="absolute border border-gray-300 w-14"></span>
+            <h1 class="text-sm tracking-widest text-gray-400">TESTIMONIAL</h1>
+        </div>
+        <h1 class="text-2xl font-semibold text-darken lg:pr-40">What They Say?</h1>
+        <p class="my-5 text-gray-500 lg:pr-36">Skilline has got more than 100k positive ratings from our users around the world.</p>
+        <p class="my-5 text-gray-500 lg:pr-36">Some of the students and teachers were greatly helped by the Skilline.</p>
+        <p class="my-5 text-gray-500 lg:pr-36">Are you too? Please give your assessment</p>
+        <button class="flex items-center pl-3 my-4 space-x-3 font-medium text-yellow-500 transition duration-300 ease-in-out transform border-t border-b border-l border-yellow-500 rounded-full focus:outline-none hover:scale-110">
+            <span>Write your assessment</span>
+            <div class="flex items-center justify-center border border-yellow-500 rounded-full h-14 w-14">
+                <svg class="w-5 h-5" viewBox="0 0 26 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M25.7071 8.70711C26.0976 8.31658 26.0976 7.68342 25.7071 7.2929L19.3431 0.928934C18.9526 0.538409 18.3195 0.538409 17.9289 0.928934C17.5384 1.31946 17.5384 1.95262 17.9289 2.34315L23.5858 8L17.9289 13.6569C17.5384 14.0474 17.5384 14.6805 17.9289 15.0711C18.3195 15.4616 18.9526 15.4616 19.3431 15.0711L25.7071 8.70711ZM-8.74228e-08 9L25 9L25 7L8.74228e-08 7L-8.74228e-08 9Z" fill="#F48C06" />
+                </svg>
+            </div>
+        </button>
+    </div>
+    <div data-aos="zoom-in-left" class="md:w-7/12">
+        <img class="mx-auto md:w-10/12" src="img/product-tour.svg">
+    </div>
+</div>
